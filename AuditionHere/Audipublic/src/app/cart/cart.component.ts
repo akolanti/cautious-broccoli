b0f00c417740232d7b2bf2995ed6e3954209133a
Mitali Patel <amit.mani128@gmail.com>
2020-12-15 00:08:12 -0500

    cart

--- AuditionHere/Audipublic/src/app/cart/cart.component.ts
+++ AuditionHere/Audipublic/src/app/cart/cart.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
+  cart:Array<object>=[];
 
   constructor() { }
 
   ngOnInit() {
+    this.cart = JSON.parse(localStorage.getItem("cart"));
   }
+  onDelete(i)
+  {
+    this.cart=this.cart.splice(i, 1);
+    this.ngOnInit();
 
+  }
 }
